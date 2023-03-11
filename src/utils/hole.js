e5f99fa15d2670275d3f319469e79c8066fdeda6
fyrodems <fyrodems@gmail.com>
2023-03-11 01:53:08 +0100

    generate new hole position each time

--- src/utils/hole.js
+++ src/utils/hole.js
@@ -1,9 +1,6 @@
-const generateLeftPositionForHole =
-  Math.floor(Math.random() * (window.innerWidth - 500)) + 400;
-
-const addHole = () => {
+export default () => {
   const hole = document.querySelector(".hole");
+  const generateLeftPositionForHole =
+    Math.floor(Math.random() * (window.innerWidth - 500)) + 400;
   hole.style.left = generateLeftPositionForHole + "px";
 };
-
-export default addHole;

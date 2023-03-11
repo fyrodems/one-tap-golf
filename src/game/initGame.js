33fd36e87585e1bb1bdf1db06bc0febbaf386e84
fyrodems <fyrodems@gmail.com>
2023-03-11 01:43:36 +0100

    optimization of game init events

--- src/game/initGame.js
+++ src/game/initGame.js
@@ -8,22 +8,21 @@ const drawPathAndFlightBall = () => {
 
 function initEvents() {
   function keyDownHandler(e) {
-    if (!e.keyCode === 32 && player.ballIsFlyingNow) return;
-    document.addEventListener("mousedown", () => {
-      const interwal = setInterval(() => {
-        console.log("%cinterwal", "color: blue");
-        drawPathAndFlightBall();
+    if (e.keyCode !== 32 || player.ballIsFlyingNow) return;
+    let interval = setInterval(() => {
+      console.log("%cinterval", "color: blue");
+      drawPathAndFlightBall();
+    }, 50);
 
-        document.addEventListener("mouseup", () => {
-          console.log("%cclear interwal", "color: red");
-
-          clearInterval(interwal);
-        });
-      }, 50);
-    });
-    document.addEventListener("mouseup", () => {
-      player.flyingBall();
-    });
+    document.addEventListener(
+      "mouseup",
+      () => {
+        console.log("%cclear interval", "color: red");
+        clearInterval(interval);
+        player.flyingBall();
+      },
+      { once: true }
+    );
     //   if (e.keyCode === 32 && !player.ballIsFlyingNow) {
     //     shootingPath.updateFlightPath();
     //   }

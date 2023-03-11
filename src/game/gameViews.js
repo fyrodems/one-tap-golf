bc3107c56ac595287f1122d2a7c3307c8c52f1bf
fyrodems <fyrodems@gmail.com>
2023-03-11 02:27:55 +0100

    add new ctx text

--- src/game/gameViews.js
+++ src/game/gameViews.js
@@ -24,7 +24,17 @@ function resetPoints() {
 function pressButtonToStart() {
   ctx1.font = "bolder 40px Arial";
   ctx.fillStyle = "yellow";
-  ctx.fillText("press Space to start", ctx.canvas.width / 2 - 230, 250);
+  ctx.fillText("", ctx.canvas.width / 2 - 230, 250);
+  ctx.fillText(
+    "To play, press and hold down your mouse button,",
+    ctx.canvas.width / 2 - 490,
+    300
+  );
+  ctx.fillText(
+    "then release it to launch the ball.",
+    ctx.canvas.width / 2 - 330,
+    350
+  );
 }
 
 function addGameOverText() {
